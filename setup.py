5d90cb5cf830743ad67cc8c88c6af5c4bd5d65d2
Craig Quiter <cquiter@gmail.com>
2019-09-29 15:03:36 -0700

    Add python box req

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-setup(name='gym_match_input_continuous',
-      version='0.0.1',
-      install_requires=['gym']  # And any other dependencies we need
+setup(
+    name='gym_match_input_continuous',
+    version='0.0.1',
+
+    # And any other dependencies we need
+    install_requires=['gym', 'python-box']
 )
